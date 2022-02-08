09d04b15278df275e2261f8f944a894a934d5f2b
jdromero88 <jdromerov88@gmail.com>
2022-02-08 10:54:24 -0500

    added a table

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -4,10 +4,43 @@
 </script>
 
 <Search satellites = {satellites} />
-<table>
-
-</table>
-
+<div>
+  <table>
+    <thead>
+      <tr>
+        <th colspan="6">Titulo de la tabla!</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>uno</td>
+        <td>dos</td>
+        <td>tres</td>
+        <td>cuatro</td>
+        <td>cinco</td>
+        <td>seis</td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td colspan="6">este es el footer!</td>
+      </tr>
+    </tfoot>
+  </table>
+</div>
 <style>
-  
+  div {
+    display: flex;
+    justify-content: center;
+  }
+  table,
+  td {
+      border: 1px solid #333;
+  }
+
+  thead,
+  tfoot {
+      background-color: #333;
+      color: #fff;
+  }
 </style>
