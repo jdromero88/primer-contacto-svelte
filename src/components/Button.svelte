1f5ca5cb38125b8ff27e6bd4388b3e48bdf2749f
jdromero88 <jdromerov88@gmail.com>
2022-02-28 20:34:16 -0500

    add tipy

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,9 +1,18 @@
 <script>
+  import tippy from "sveltejs-tippy";
+
   export let handleClick
   export let text = 'Click Me'
+
+  // Handle tippy
+  const tippyProps = {
+    content: text,
+    placement: "bottom"
+  };
+
 </script>
 
-<button on:click={handleClick}>
+<button use:tippy={tippyProps} on:click={handleClick}>
   {text}!
 </button>
 
